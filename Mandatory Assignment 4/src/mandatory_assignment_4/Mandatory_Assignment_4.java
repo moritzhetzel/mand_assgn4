37fb5ac5a9e55fb04ffa6315a2f6adb3332da5d5
moritzhetzel <moritzhetzel@moritzs-mbp-3.eduroam.wireless.dtu.dk>
2018-03-07 11:24:24 +0100

    New Version

--- Mandatory Assignment 4/src/mandatory_assignment_4/Mandatory_Assignment_4.java
+++ Mandatory Assignment 4/src/mandatory_assignment_4/Mandatory_Assignment_4.java
@@ -37,10 +37,16 @@ class Board {
 		System.out.println(Arrays.deepToString(board).replace("], ", "\n").replace("[[", "").replace("]]", "").replace("[",  "").replace(",", ""));
 	}
 	
+	String get(int x, int y) {
+		return board[y+2][x+2];
+	}
+	
 }
 
 
-abstract class Token extends Board {
+abstract class Token {
+	
+	Board board;
 	
 	int x1;					//Coordinates of token
 	int y1;
@@ -53,6 +59,8 @@ abstract class Token extends Board {
 	int x2;					//New coordinates of token
 	int y2;
 	
+	boolean exception;
+	
 	Scanner s = new Scanner(System.in);
 	
 	Token() {
@@ -60,27 +68,20 @@ abstract class Token extends Board {
 		this.y1 = 0;
 	}
 	
-	void enterCoordinates() {
-		System.out.print("Enter X: ");
-		
-		x1 = s.nextInt();
-		
-		System.out.print("Enter Y: ");
-		
-		y1 = s.nextInt();
-	}
-	
-	abstract boolean checkValidity1();
-	
+	abstract void enterCoordinates() throws MyException;
 	abstract void possiblePositions();
 	
+	void setBoard(Board x) {
+		board = x;
+	}
+	
 	boolean checkOccupation(int yp, int xp) {						//false if occupied
-		if ( board[yp+2][xp+2].equals("1") || board[yp+2][xp+2].equals("2") ) {
+		if ( board.get(xp, yp).equals("1") || board.get(xp, yp).equals("2") ) {
 			return false;
 		} else { return true; }
 	}
 	
-	void enterNewCoordinates() {
+	void enterNewCoordinates() throws MyException {
 		System.out.print("Enter X: ");
 		
 		x2 = s.nextInt();
@@ -88,21 +89,17 @@ abstract class Token extends Board {
 		System.out.print("Enter Y: ");
 		
 		y2 = s.nextInt();
-	}
-	
-	boolean checkValidity2() {
-		try {
-			if ( !( ((x2 == 0) || (x2 == 1) || (x2 == 2) || (x2 == 3) || (x2 == 4) || (x2 == 5) || (x2 == 6) || (x2 == 7)) && ((y2 == 0) || (y2 == 1) || (y2 == 2) || (y2 == 3) || (y2 == 4) || (y2 == 5) || (y2 == 6) || (y2 == 7)) ) ) {
-				throw new MyException("Invalid coordinate!");
+		
+		if ( !( ((x2 == 0) || (x2 == 1) || (x2 == 2) || (x2 == 3) || (x2 == 4) || (x2 == 5) || (x2 == 6) || (x2 == 7)) && ((y2 == 0) || (y2 == 1) || (y2 == 2) || (y2 == 3) || (y2 == 4) || (y2 == 5) || (y2 == 6) || (y2 == 7)) ) ) {
+			exception = true;
+			throw new MyException("Invalid coordinate!");
+		} else {
+			if ( (this.checkOccupation(yp1, xp1) && (x2 == xp1) && (y2 == yp1)) || (this.checkOccupation(yp2, xp2) && (x2 == xp2) && (y2 == yp2)) ) {
+				exception = false;
 			} else {
-				if ( (this.checkOccupation(yp1, xp1) && (x2 == xp1) && (y2 == yp1)) || (this.checkOccupation(yp2, xp2) && (x2 == xp2) && (y2 == yp2)) ) {
-					return true;
-				} else { throw new MyException("Invalid coordinate!"); }
-			}
-		}
-		catch (MyException e) {
-			System.out.println("Invalid coordinate! Try again!");
-			return false;
+				exception = true;
+				throw new MyException("Invalid coordinate!");
+				}
 		}
 	}
 	
@@ -114,19 +111,25 @@ abstract class Token extends Board {
 class Token_1 extends Token {
 	
 	@Override
-	boolean checkValidity1() {
-		try {
-			if ( !( ((x1 == 0) || (x1 == 1) || (x1 == 2) || (x1 == 3) || (x1 == 4) || (x1 == 5) || (x1 == 6) || (x1 == 7)) && ((y1 == 0) || (y1 == 1) || (y1 == 2) || (y1 == 3) || (y1 == 4) || (y1 == 5) || (y1 == 6) || (y1 == 7)) ) ) {
-				throw new MyException("Invalid coordinate!");
+	void enterCoordinates() throws MyException{
+		System.out.print("Enter X: ");
+		
+		x1 = s.nextInt();
+		
+		System.out.print("Enter Y: ");
+		
+		y1 = s.nextInt();
+		
+		if ( !( ((x1 == 0) || (x1 == 1) || (x1 == 2) || (x1 == 3) || (x1 == 4) || (x1 == 5) || (x1 == 6) || (x1 == 7)) && ((y1 == 0) || (y1 == 1) || (y1 == 2) || (y1 == 3) || (y1 == 4) || (y1 == 5) || (y1 == 6) || (y1 == 7)) ) ) {
+			exception = true;
+			throw new MyException("Invalid coordinate!");
+		} else {
+			if ( (board.get(x1, y1)).equals("1") ) {
+				exception = false;
 			} else {
-				if ( board[y1+2][x1+2].equals("1") ) {
-					return true;
-				} else { throw new MyException("Invalid coordinate!"); }
-			}
-		}
-		catch (MyException e) {
-			System.out.println("Invalid coordinate! Try again!");
-			return false;
+				exception = true;
+				throw new MyException("Invalid coordinate!");
+				}
 		}
 	}
 	
@@ -141,8 +144,8 @@ class Token_1 extends Token {
 
 	@Override
 	void moveToken() {
-		board[y1+2][x1+2] = " ";
-		board[y2+2][x2+2] = "1";
+		board.board[y1+2][x1+2] = " ";
+		board.board[y2+2][x2+2] = "1";
 	}
 	
 }
@@ -151,19 +154,24 @@ class Token_1 extends Token {
 class Token_2 extends Token {
 	
 	@Override
-	boolean checkValidity1() {
-		try {
-			if ( !( ((x1 == 0) || (x1 == 1) || (x1 == 2) || (x1 == 3) || (x1 == 4) || (x1 == 5) || (x1 == 6) || (x1 == 7)) && ((y1 == 0) || (y1 == 1) || (y1 == 2) || (y1 == 3) || (y1 == 4) || (y1 == 5) || (y1 == 6) || (y1 == 7)) ) ) {
-				throw new MyException("Invalid coordinate!");
+	void enterCoordinates() throws MyException{
+		System.out.print("Enter X: ");
+		
+		x1 = s.nextInt();
+		
+		System.out.print("Enter Y: ");
+		
+		y1 = s.nextInt();
+		
+		if ( !( ((x1 == 0) || (x1 == 1) || (x1 == 2) || (x1 == 3) || (x1 == 4) || (x1 == 5) || (x1 == 6) || (x1 == 7)) && ((y1 == 0) || (y1 == 1) || (y1 == 2) || (y1 == 3) || (y1 == 4) || (y1 == 5) || (y1 == 6) || (y1 == 7)) ) ) {
+			exception = true;
+			throw new MyException("Invalid coordinate!");
+		} else {
+			if ( board.get(x1, y1).equals("2") ) {
+				exception = false;
 			} else {
-				if ( board[y1+2][x1+2].equals("2") ) {
-					return true;
-				} else { throw new MyException("Invalid coordinate!"); }
-			}
-		}
-		catch (MyException e) {
-			System.out.println("Invalid coordinate! Try again!");
-			return false;
+				exception = true;
+				throw new MyException("Invalid coordinate!"); }
 		}
 	}
 	
@@ -178,11 +186,10 @@ class Token_2 extends Token {
 
 	@Override
 	void moveToken() {
-		board[y1+2][x1+2] = " ";
-		board[y2+2][x2+2] = "2";
+		board.board[y1+2][x1+2] = " ";
+		board.board[y2+2][x2+2] = "2";
 	}
 }
-
 
 
 
@@ -204,6 +211,8 @@ public class Mandatory_Assignment_4 {
 		Token_1 token_1 = new Token_1();
 		Token_2 token_2 = new Token_2();
 		
+		token_1.setBoard(board);
+		token_2.setBoard(board);
 		
 		do {
 			
@@ -222,10 +231,15 @@ public class Mandatory_Assignment_4 {
 		//Check if right token is on the entered coordinates
 		
 		//Token_1 token_1 = new Token_1();
-		
+
 		do {
-			token_1.enterCoordinates();
-		} while (!(token_1.checkValidity1()));
+			try {
+				token_1.enterCoordinates();
+			}
+			catch (MyException e) {
+				System.out.println("Invalid coordinate! Try again!");
+			}
+		} while (token_1.exception);
 		
 		
 		//Calculate possible moves
@@ -240,8 +254,13 @@ public class Mandatory_Assignment_4 {
 		System.out.println("\n" + "Coordinates of new position");
 		
 		do {
-			token_1.enterNewCoordinates();
-		} while (!(token_1.checkValidity2()));
+			try {
+				token_1.enterNewCoordinates();
+			}
+			catch (MyException e) {
+				System.out.println("Invalid coordinate! Try again!");
+			}
+		} while (token_1.exception);
 		
 		
 		//Move token to new position
@@ -268,8 +287,13 @@ public class Mandatory_Assignment_4 {
 		//Check if right token is on the entered coordinates
 		
 		do {
-			token_2.enterCoordinates();
-		} while (!(token_2.checkValidity1()));
+			try {
+				token_2.enterCoordinates();
+			}
+			catch (MyException e) {
+				System.out.println("Invalid coordinate! Try again!");
+			}
+		} while (token_2.exception);
 		
 		
 		//Calculate possible moves
@@ -284,8 +308,13 @@ public class Mandatory_Assignment_4 {
 		System.out.println("\n" + "Coordinates of new position");
 				
 		do {
-			token_2.enterNewCoordinates();
-		} while (!(token_2.checkValidity2()));
+			try {
+				token_2.enterNewCoordinates();
+			}
+			catch (MyException e) {
+				System.out.println("Invalid coordinate! Try again!");
+			}
+		} while (token_2.exception);
 		
 		
 		//Move token to new position
